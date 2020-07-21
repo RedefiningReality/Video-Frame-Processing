d68308c3fa350c0fe034587d1aaad7627fe9e62b
TheCatLover <johnthejolly@gmail.com>
2020-07-21 13:56:15 -0500

    Update write_frames.py

--- write_frames.py
+++ write_frames.py
@@ -1,27 +1,27 @@
 import cv2, sys, os
 
 if len(sys.argv) != 1:
-  print('Usage: python/python3 write_frames.py <directory with images>')
-  exit()
+	print('Usage: python/python3 write_frames.py <directory with images>')
+	exit()
 
 directory = sys.argv[1]
 if not os.path.exists(directory) or not os.path.isdir(directory):
-  print(directory + ' is not a valid directory')
-  exit()
+	print(directory + ' is not a valid directory')
+	exit()
 
 video = input('Output video name: ')
 fourcc = input('Output video encoding: ')
 try:
-  fourcc = int(fourcc)
+	fourcc = int(fourcc)
 except ValueError:
-  print(fourcc + ' must be an integer')
-  exit()
+	print(fourcc + ' must be an integer')
+	exit()
 fps = input('Output video frames per second: ')
 try:
-  fps = float(fps)
+	fps = float(fps)
 except ValueError:
-  print(fps + ' must be a float')
-  exit()
+	print(fps + ' must be a float')
+	exit()
 
 # Obtains image files in directory
 files = [img for img in os.listdir(directory) if img.endswith('.png') or img.endswith('.jpg')]
@@ -33,17 +33,17 @@ height, width, _ = frame.shape
 size = (width, height)
 
 def save_video(vid_name, framerate):
-  vid = cv2.VideoWriter(vid_name, fourcc, framerate, size)
-
+	vid = cv2.VideoWriter(vid_name, fourcc, framerate, size)
+	
 	for file in files:
 		print('Adding frame ' + file)
 		img = cv2.imread(os.path.join(directory, file))
 		vid.write(img)
 	print('All frames added')
-
+	
 	cv2.destroyAllWindows()
 	vid.release()
-  print('Video ' + vid_name + ' created successfully')
+	print('Video ' + vid_name + ' created successfully')
 
 save_video(video + '.avi', fps)
 # Feel free to use the save_video method to create different
